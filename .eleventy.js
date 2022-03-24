d6e3512199b28ac8aea917455e7b5597143c4a7f
LeeShin <lslooa@gmail.com>
2022-03-23 17:18:42 -0700

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = eleventyConfig => {
+
+    //const markdownIt = require('markdown-it')
+   // const markdownItAttrs = require('markdown-it-attrs')
+  
+    /*const markdownItOptions = {
+      html: true,
+      breaks: true,
+      linkify: true
+    }
+
+    const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+
+    
+    eleventyConfig.setLibrary('md', markdownLib);
+    eleventyConfig.markdownTemplateEngine = 'njk';
+*/
+
+eleventyConfig.addPassthroughCopy("src/assets");
+eleventyConfig.markdownTemplateEngine = 'njk';
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
